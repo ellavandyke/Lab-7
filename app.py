412fb4efc6bf7c16cc72370931e10b3fa03e45bc
ellavandyke <spb115@mocs.utc.edu>
2025-02-28 22:13:01 -0500

    app.py

--- app.py
+++ app.py
@@ -53,12 +53,12 @@ st.write("Enter property details to predict house price.")
 
 # User input fields
 bedrooms = st.number_input("Bedrooms", min_value=1, max_value=8, value=4)
-bathooms = st.number_input("Bathrooms", min_value=1, max_value=4, value=2)
+bathrooms = st.number_input("Bathrooms", min_value=1, max_value=4, value=2)
 lot_area = st.number_input("Lot Area sf", min_value=1300, max_value=215245, value=100000)
 year_built = st.number_input("Year Built", min_value=1900, max_value=2025, value=2000)
 
 # Predict price based on user input
-input_data = pd.DataFrame([[bedrooms, bathrooms, lot_area, year_built]], columns=features)
+input_data = pd.DataFrame([[bedrooms,bathrooms,lot_area,year_built]], columns=features)
 predicted_price = model.predict(input_data)[0]
 
 st.write(f"Predicted Sale Price: ${predicted_price:,.2f}")
